2f975ee37d2c73ef944b2cf7b849de5370ede5e3
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2023-12-09 12:27:16 +0200

    introduction to python

---
+++ datatypes.py
@@ -0,0 +1,90 @@
+
+
+# name=  'ahmed'
+# track = 'ai'
+# grade = 100
+# satisfied =  True
+# g = 9.8
+#
+# "1- get datatype of any variable"
+# print(type(track))
+# print(type(satisfied))
+# print(type(g))
+#
+# """2- conversion between different types --> casting """
+# print(grade, type(grade))
+# grade = str(grade)
+# print(grade, type(grade))
+#
+#
+# # name = bool(name)
+# #
+# # test  = 10 ## value represent True, or False
+# #
+# # test = bool(test)
+# # print(test, type(test))
+#
+# """ boolean datatype ---> True , False"""
+#
+# """ boolean ---> 1 --> True, 0 --> False """
+#
+# """----- datatypes conversions """
+#
+# """1- convert string/ int/ float to boolean
+#
+#     var = bool(var)
+#
+# """
+# var = 0
+#
+# """2- convert int/float to string """
+# g= str(g)
+# print(g)
+#
+# """3- convert string to int/float """
+# year = '2023' # string
+# print(year, type(year))
+# year = int(year)
+# print(year, type(year))
+#
+# # name = "Marina"
+# # name =int(name)
+# # print(name, type(name))
+#
+# """ size of empty string in python ---> 49 byte
+# size of integer ? byte
+# """
+#
+# """ conversion between string and int  --> valid only if string contains numeric value
+#
+#     '555'
+# """
+#
+# # grade = '55'
+# # print(type(grade))
+#
+# # address ='25iti'
+# # print(type(address))
+# # address = int(address)
+# # print(type(address))
+#
+#
+#
+# # num = 34567893827487487394789379837489374
+#
+#
+# """ datatypes and logical operators """
+#
+# print("ahmed" and "iti")
+
+res = 'ahmed' and 'iti'
+print(res)
+
+res = '' and 'ali'
+print(res)
+
+name='noha'
+if name and '':
+    print('hii')
+else:
+    print("bye")
---
+++ intro.py
@@ -0,0 +1,63 @@
+# print("....hello Ghaza ....")
+# name = 'ahmed'
+# Name = 'Ali'
+#
+# # and = 10
+# # print= 10
+# # print(name)
+# # print(name, and)
+#
+# # this is a comment in python
+# # no difference between single or double quoted string.
+#
+# # you can use the string as a comment
+# 'this is a string can be treated as a a comment  '
+#
+# """no braces in python just indentations """
+#
+# """each python line must start at the beginning of the line except lines in blocks """
+#
+# print("Hello")
+#
+# name = 'ahmed'
+# if name == 'ahmed':
+#     print("Hi")
+#     if name[0]=='a':
+#         print("--test---")
+# else:
+#     print("bye")
+#
+# # track = 'Ai'
+# # track2 = "Artificial Intelligence"
+#
+# bio = ("My name is Noha"
+#        "I works at ITI"
+#        "I lives in Mansoura")
+#
+# print(bio)
+#
+# """to keep newlines in the string """
+# bio= '''My name is Noha
+# I works at ITI
+# I lives in Mansoura'''
+#
+# print(bio)
+#
+# """ when you define variable in python ---> reserve new location in the memory
+# during runtime , you need to define datatype when define variable,
+# python detect datatype in the runtime  """
+#
+# # age = 31
+# # grade = 33.33
+# # salary=1000
+#
+# """ where is main in python
+#
+# each python module. .py file has an entry point for main --- __name__='__main__'
+# and this entrypoint helps the interpreter to start the execution
+# """
+#
+# """ limit someparts to run only when running from the file """
+#
+# if __name__ == '__main__':
+#     print("---- running only what is inside the file")
